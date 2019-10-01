34604ae9dcdd0adf848de29cf0381a08624a094a
Levent Cetiner <overandover@gmail.com>
2019-10-01 13:39:06 +0200

    + update tag styling option to have them fit inside the profile thumbnail

--- src/components/ProfileWall/profiles.js
+++ src/components/ProfileWall/profiles.js
@@ -4,12 +4,28 @@ import Gallery from 'react-grid-gallery';
 
 const Profiles = (props) => {
     console.log("props", toJS(props.profileList));
+    const tagStyle = {
+        display: "inline",
+        padding: "0.2em 0.6em 0.3em",
+        fontSize: "47%",
+        fontWeight: "600",
+        lineHeight: "8",
+        color: "yellow",
+        background: "rgba(0, 0, 0, 0.65)",
+        textAlign: "center",
+        whiteSpace: "nowrap",
+        verticalAlign: "baseline",
+        borderRadius: "0.25em",
+        boxShadow: "0 0 black",
+        backgroundColor: "#00415b",
+    };
 
     return(
         <div>
             <Gallery
                 images={props.profileList}
                 enableImageSelection={false}
+                tagStyle={tagStyle}
                 margin={15}
             />
         </div>
