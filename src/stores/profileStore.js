025975473ae47979ac1fa4cde8ee6b6c4adbeb31
Levent Cetiner <overandover@gmail.com>
2019-10-01 15:18:11 +0200

    + add last logged in information inside the detail view

--- src/stores/profileStore.js
+++ src/stores/profileStore.js
@@ -1,4 +1,4 @@
-import { action,observable,toJS } from 'mobx';
+import { action,observable } from 'mobx';
 import _ from 'lodash';
 
 // Import API
@@ -14,6 +14,7 @@ export default class ProfileStore {
     }
 
     static instance = () => (ProfileStore._instance = ProfileStore._instance || new ProfileStore());
+
     /**
      * LOAD ProfileWall ACTIONS
      */
@@ -27,9 +28,8 @@ export default class ProfileStore {
     }
 
     @action
-    loadProfilesSuccess(result,params) {
+    loadProfilesSuccess(result) {
         this.profiles=  _.size(result.items) < 36 ? result.items : _.merge(this.profiles,result.items);
-
         this.loadDetailedProfiles();
     }
 
@@ -45,8 +45,13 @@ export default class ProfileStore {
 
     @action
     loadDetailedProfilesSuccess(result) {
+        const currentDate = new Date();
+        const one_day = 1000*60*60;
 
+        // set profiles array as the react gallery requires
         this.profiles = _.map(_.merge(this.profiles,result),profile => {
+            const lastLogin = new Date(profile.last_login)
+            const passedTime = Math.round((currentDate - lastLogin) / one_day)
           return {
               src: _.get(profile,'picture.url','http://localhost:5000/images/noImage.png'),
               thumbnail: _.get(profile,'picture.url','http://localhost:5000/images/noImage.png'),
@@ -55,16 +60,10 @@ export default class ProfileStore {
               thumbnailCaption: profile.name,
               caption:`${profile.headline}`,
               locationName: _.get(profile,'location.name'),
+              lastLogin:passedTime,
               tags: [{value:_.get(profile,'personal.age'), title: _.get(profile,'personal.age')}, {value: profile.online_status, title: profile.online_status}, {value:`${_.get(profile,'location.distance')}m`, title:`${_.get(profile,'location.distance')}m`}],
-
           }
-        })
-
-        // this.profiles = result
-        console.log("r24", toJS(this.profiles),toJS(result));
+        });
     }
-
-
-
 }
 
